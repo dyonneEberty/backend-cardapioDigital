d6377b8b2d8f9c8aeee816c4284c81586b016df2
dyonneEberty <bydyonne@gmail.com>
2024-12-21 20:40:36 -0300

    first commit

--- src/main/java/com/example/cardapio/food/Food.java
+++ src/main/java/com/example/cardapio/food/Food.java
@@ -1,9 +1,11 @@
 package com.example.cardapio.food;
 
 import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 
 @Table(name = "foods")
 @Entity(name = "foods")
+@EqualsAndHashCode(of = "id")
 public class Food {
 
     @Id
@@ -12,4 +14,29 @@ public class Food {
     private String title;
     private String image;
     private Integer price;
+
+    public Food(Long id, String title, String image, Integer price) {
+        this.id = id;
+        this.title = title;
+        this.image = image;
+        this.price = price;
+    }
+
+    public Food(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
 }
