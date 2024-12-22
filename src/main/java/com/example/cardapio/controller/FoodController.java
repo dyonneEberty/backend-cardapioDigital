4b1a8f3c0f46c7b1c6bf186885f1d5c67a8ab882
dyonneEberty <bydyonne@gmail.com>
2024-12-22 09:46:48 -0300

    Criação do método POST para adição de Food no cardapio

--- src/main/java/com/example/cardapio/controller/FoodController.java
+++ src/main/java/com/example/cardapio/controller/FoodController.java
@@ -3,9 +3,9 @@ package com.example.cardapio.controller;
 import com.example.cardapio.food.Food;
 import com.example.cardapio.food.FoodRepository;
 import com.example.cardapio.food.FoodResponseDTO;
+import com.example.cardapio.food.foodRequestDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,6 +16,16 @@ public class FoodController {
     @Autowired
     private FoodRepository repository;
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping
+    public void saveFood(@RequestBody foodRequestDTO data) {
+        Food foodData = new Food(data);
+        repository.save(foodData);
+        return;
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping
     public List<FoodResponseDTO> getAll(){
 
         List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
